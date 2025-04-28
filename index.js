6d691b6bf0c9a178719c5061fb54991eeb2c39f6
amberitas17 <amberclarisse0928@gmail.com>
2025-04-28 15:35:22 +0800

    hello

--- index.js
+++ index.js
@@ -419,7 +419,7 @@ if (!vectorStoreId && !isExcelOrCsv) {
                 fs.mkdirSync(downloadsDir, { recursive: true });
               }
       
-                const fileUrl = `https://azure2234.openai.azure.com/openai/files/${fileId}/content?api-version=2024-05-01-preview`;
+                const fileUrl = `https://ihisenpaiihiap1160250515.cognitiveservices.azure.com/openai/files/${fileId}/content?api-version=2024-05-01-preview`;
       
                 try {
                   const response = await fetch(fileUrl, {
@@ -438,7 +438,7 @@ if (!vectorStoreId && !isExcelOrCsv) {
                   fs.writeFileSync(destPath, buffer);
       
                   const downloadLink = process.env.AZURE === 'true'
-                    ? `https://ihisenpai23.azurewebsites.net/downloads/${path.basename(filePath)}`
+                    ? `https://ihisenpaiappihiap-ewcjhzcqdehvb9dz.southeastasia-01.azurewebsites.net/downloads/${path.basename(filePath)}`
                     : `http://localhost:${port}/downloads/${path.basename(filePath)}`;
       
                     console.log(`File downloaded to: ${destPath}`);
@@ -453,7 +453,7 @@ if (!vectorStoreId && !isExcelOrCsv) {
               await Promise.all(annotationTasks); // Process all annotations concurrently
             } else if (item.type === "image_file") {
               try {
-                const imageResponse = await fetch(`https://azure2234.openai.azure.com/openai/files/${item.image_file.file_id}/content?api-version=2024-05-01-preview`, {
+                const imageResponse = await fetch(`https://ihisenpaiihiap1160250515.cognitiveservices.azure.com/openai/files/${item.image_file.file_id}/content?api-version=2024-05-01-preview`, {
                   headers: {
                     'api-key': process.env.AZURE_OPENAI_KEY
                   }
