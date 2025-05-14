631e833ee79d6ac32fa8f9626f217656ef1fde36
amberitas17 <amberclarisse0928@gmail.com>
2025-05-14 14:27:21 +0800

    new link

--- index.js
+++ index.js
@@ -444,7 +444,7 @@ if (!vectorStoreId && !isExcelOrCsv) {
                   fs.writeFileSync(destPath, buffer);
       
                   const downloadLink = process.env.AZURE === 'true'
-                    ? `https://ihisenpaiappihiap-ewcjhzcqdehvb9dz.southeastasia-01.azurewebsites.net/downloads/${path.basename(filePath)}`
+                    ? `https://senpaiihiapv1-fcfwetbaetfzb9bb.australiaeast-01.azurewebsites.net/downloads/${path.basename(filePath)}`
                     : `http://localhost:${port}/downloads/${path.basename(filePath)}`;
       
                     console.log(`File downloaded to: ${destPath}`);
